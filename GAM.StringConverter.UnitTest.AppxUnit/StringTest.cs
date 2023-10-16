2e4b878661f3346d1dcd68f3e620df842a1735ad
Gustavo Munoz <softwareadolfo@gmail.com>
2023-10-16 12:34:46 -0600

    Prubas con Unit terminadas

---
+++ GAM.StringConverter.UnitTest.AppxUnit/StringTest.cs
@@ -0,0 +1,38 @@
+﻿
+using GAM.Temperatura;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GAM.StringConverter.UnitTest.AppxUnit
+{
+    public class StringTest
+    {
+        [Fact]
+        public void Reverse_ShouldReturn()
+        {
+            StringManipulator stringMap = new StringManipulator();
+
+            string word = "Pelicula";
+
+            string result = stringMap.ReverseString(word);
+
+            Assert.Equal("alucileP", result);
+        }
+
+        [Fact]
+        public void Space_ShouldReturn()
+        {
+            StringManipulator stringWhite = new StringManipulator();
+
+            string word = "Hay una nueva pelicula";
+
+            string result = stringWhite.RemoveSpaces(word);
+
+            Assert.Equal("Hayunanuevapelicula", result);
+        }
+    }
+}
